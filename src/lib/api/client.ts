b605a63e70911e3f7507a31615895250d0d47b94
kemal-faza <kemalfaza26@gmail.com>
2026-07-05 19:22:16 +0700

    fix: AsyncStorage.removeMany and exclude tests from tsc

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -44,5 +44,5 @@ export async function setTokens(accessToken: string, refreshToken: string) {
 }
 
 export async function clearTokens() {
-  await AsyncStorage.multiRemove(['accessToken', 'refreshToken', 'user']);
+  await AsyncStorage.removeMany(['accessToken', 'refreshToken', 'user']);
 }
